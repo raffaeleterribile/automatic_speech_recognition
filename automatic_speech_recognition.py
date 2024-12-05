8ffa11e6682ec7c3ad86ef4d7443d2c27dc1b0ce
Raffaele Terribile <raff-github@usa.net>
2024-12-05 19:09:07 +0100

    Aggiungi riconoscimento realtime da microfono

--- automatic_speech_recognition.py
+++ automatic_speech_recognition.py
@@ -1,14 +1,15 @@
 """ Automatic Speech Recognition with 🤗 Transformers and OpenAI's Whisper """
+import numpy as np
 import torch
-from transformers import pipeline
 import gradio as gr
+from transformers import pipeline
 
 MODEL_NAME = "openai/whisper-tiny"
 BATCH_SIZE = 8
 
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-pipe = pipeline(
+transcriber = pipeline(
 	task="automatic-speech-recognition",
 	model=MODEL_NAME,
 	chunk_length_s=30,
@@ -37,9 +38,39 @@ def format_timestamp(seconds: float, always_include_hours: bool = False, decimal
 		# we have a malformed timestamp so just return it as is
 		return seconds
 
-def transcribe(file, task, return_timestamps):
-	""" Transcribe audio file. """
-	outputs = pipe(file, batch_size=BATCH_SIZE, generate_kwargs={"task": task}, return_timestamps=return_timestamps)
+def transcribe_from_microphone(stream, new_chunk, task, return_timestamps):
+	""" Transcribe audio from microphone. """
+	sampling_rate, audio = new_chunk
+
+	# Convert to mono if stereo
+	if audio.ndim > 1:
+		audio = audio.mean(axis=1)
+
+	audio = audio.astype(np.float32)
+	audio /= np.max(np.abs(audio))
+
+	if stream is not None:
+		stream = np.concatenate([stream, audio])
+	else:
+		stream = audio
+
+	print(f"Transcribing {len(stream) / sampling_rate:.2f} seconds of audio with a sampling rate of {sampling_rate} Hz...")
+	outputs =  transcriber(stream, generate_kwargs={"task": task, "language": "it"},
+					   return_timestamps=return_timestamps)
+	text = outputs["text"]
+	if return_timestamps:
+		timestamps = outputs["chunks"]
+		timestamps = [
+			f"[{format_timestamp(chunk['timestamp'][0])} -> {format_timestamp(chunk['timestamp'][1])}] {chunk['text']}"
+			for chunk in timestamps
+		]
+		text = "\n".join(str(feature) for feature in timestamps)
+	print(f"Transcribed audio: {text}")
+	return stream, text
+
+def transcribe_from_file(file, task, return_timestamps):
+	""" Transcribe audio from file. """
+	outputs = transcriber(file, batch_size=BATCH_SIZE, generate_kwargs={"task": task}, return_timestamps=return_timestamps)
 	text = outputs["text"]
 	if return_timestamps:
 		timestamps = outputs["chunks"]
@@ -50,16 +81,15 @@ def transcribe(file, task, return_timestamps):
 		text = "\n".join(str(feature) for feature in timestamps)
 	return text
 
-demo = gr.Blocks()
-
 mic_transcribe = gr.Interface(
-	fn=transcribe,
+	fn=transcribe_from_microphone,
 	inputs=[
-		gr.Audio(sources="microphone", type="filepath"),
+		"state",
+		gr.Audio(sources="microphone", type="numpy", label="Audio file", streaming=True, recording=False),
 		gr.Radio(["transcribe", "translate"], label="Task", value="transcribe"),
 		gr.Checkbox(value=False, label="Return timestamps"),
 	],
-	outputs="text",
+	outputs=["state", "text"],
 	theme="default",
 	title="Whisper Demo: Transcribe Audio",
 	description=(
@@ -68,12 +98,13 @@ mic_transcribe = gr.Interface(
 		" of arbitrary length."
 	),
 	flagging_mode="never",
+	live=True
 )
 
 file_transcribe = gr.Interface(
-	fn=transcribe,
+	fn=transcribe_from_file,
 	inputs=[
-		gr.Audio(sources="upload", label="Audio file"),
+		gr.Audio(sources="upload", type="filepath", label="Audio file"),
 		gr.Radio(["transcribe", "translate"], label="Task", value="transcribe"),
 		gr.Checkbox(value=False, label="Return timestamps"),
 	],
